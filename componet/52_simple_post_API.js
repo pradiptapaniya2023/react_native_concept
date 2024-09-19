00e5b053c4e202ad3a992170cb157aea483e50bc
Pradip Tapaniya <pradiptapaniya2023.dev@gmail.com>
2024-09-19 22:21:02 +0530

    update19

---
+++ componet/52_simple_post_API.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+const SimplePostAPI = () => {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+export default SimplePostAPI;
